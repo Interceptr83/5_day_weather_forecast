a43d1a87161e7d9959852bc214b3b64d7ef45735
Dustin Smith <dustinsmith1983@gmail.com>
2022-01-28 12:14:15 -0800

    working modifications to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,58 @@
-let searchEl = document.getElementById("searchFld");
-let submitEl = document.getElementById("searchBut");
+var searchHistory = [];
+var apiUrl = 'https://api.openweathermap.org';
+var apiKey = '5c19ae264ab26bc706f25f3b4635f591';
 
-$(document).ready(function () {
-        $('#currentTime').text(moment().format('MMMM Do YYYY, h:mm a'));
+
+let submitEl = document.getElementById("searchBut");
+let searchEl = document.getElementById("searchFld");
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+/* const date = dayjs()tz(timeone).format('M/D/YYYY'); */
+
+
+
+           
+
+function getLatLon(search){
+var url = `${apiUrl}/geo/1.0/direct?q${search}&limit=5&appid=${apiKey}`;
+fetch(apiUrl)
+    .then(function (res) {
+      console.log(res.json());;
+      return res.json();
+    })
+    .then(function (data) {
+      if (!data[0]) {
+        alert('Location not found');
+      } else {
+        appendToHistory(search);
+        fetchWeather(data[0]);
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+
+          
+};     
 
 function citySearch(){
-        const content = document.getElementById("content");
-        content.removeAttribute("class", "hidden");
+          
+    if (!searchEl.value) {
+      return;
+    }
+          
+    var search = searchEl.value.trim();
+    getLatLon(search);
+    searchEl.value = '';
+
+    const content = document.getElementById("content");
+    content.removeAttribute("class", "hidden");
 };
 
-        submitEl.onclick = citySearch;
-});
+
+submitEl.onclick = citySearch;
+/* createHistory();
+searchForm.addEventListener('submit', handleSearchFormSubmit);
+searchHistoryContainer.addEventListener('click', handleSearchHistoryClick);  */
